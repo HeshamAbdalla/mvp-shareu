568b4c92db74b3ef276906e825b74496cfed6fc5
heshamabdalla <shamoh702@gmail.com>
2016-10-18 00:18:51 -0700

    Connecting Mongod to heroku cluster

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -18,6 +18,6 @@ module.exports = {
     uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/snapit'
+            'mongodb://heroku_f9z5cjj2:an3cmlerjjhi0m5erb6t6qkdej@ds061076.mlab.com:61076/heroku_f9z5cjj2'
   }
 };
