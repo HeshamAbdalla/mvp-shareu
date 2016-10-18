debe4f43fa46cd8506d6acf2ea0f6547931e055c
heshamabdalla <shamoh702@gmail.com>
2016-10-18 11:13:30 -0700

    changed nav-bar style added scraper folder

--- server/routes.js
+++ server/routes.js
@@ -11,7 +11,8 @@ var path = require('path');
 module.exports = function(app) {
 
   // Insert routes below
-  app.use('/api/look' require('./api/look'));
+  app.use('/api/look', require('./api/look'));
+  app.use('/api/links', require('./api/imgScraper'));
   app.use('/api/users', require('./api/user'));
   app.use('/auth', require('./auth'));
   app.post('/forgotpassword', require('./forgotpassword').reset);
